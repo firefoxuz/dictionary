84d6e0f83bc8a460561eeea8049bb53adc031b80
Sultonov Daler <daler.sultonov.00@gmail.com>
2022-02-10 10:54:23 +0500

    Initial commit

---
+++ resources/lang/en/data.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'site' => [
+        'title' => 'O\'zbek tilining izohiy lug\'ati',
+        'login' => [
+            'sign_in' => 'Tizimga kirish',
+            'enter' => 'Kirish',
+            'email' => 'Potcha manzili',
+            'password' => 'Parol',
+            'remember' => 'Eslab qolish',
+        ],
+        'logout' => 'Chiqish'
+    ],
+    'table' => [
+        'count' => 'Soni',
+        'actions' => 'Harakatlar',
+        'delete' => 'O\'chirish',
+        'save' => 'Saqlash'
+    ],
+    'user' => [
+        'user' => 'Tizim foydalanuvchilari',
+        'list' => 'Ro\'yxat',
+        'new' => 'Foydalanuvchi qo\'shish',
+        'full_name' => 'F.I.O',
+        'email' => 'Email',
+        'date' => 'Sana'
+    ],
+];
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/login', ['App\Http\Controllers\Admin\User\Login\LoginController', 'index'])->name('auth.login');
+Auth::routes([
+    'register' => false,
+    'reset' => false,
+    'verify' => false,
+    'confirm' => false,
+]);
+
+
+Route::group(['middleware' => ['auth', 'disable.caching.on.local']], function () {
+    Route::get('/', ['App\Http\Controllers\Admin\Home\HomeController', 'index'])->name('home');
+    Route::get('/language/{name}', ['App\Http\Controllers\Admin\Language\LanguageController', 'setLanguage'])->name('lang');
+    Route::resource('/user','App\Http\Controllers\Admin\User\UserController');
+});
